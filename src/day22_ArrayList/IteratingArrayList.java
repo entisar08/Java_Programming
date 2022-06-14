ae552bbfc93c36e20640b7ef8da2813d817ebdf6
entisar08 <eelmohbat@gmail.com>
2022-06-14 18:46:26 -0500

    day22_ArrayList

---
+++ src/day22_ArrayList/IteratingArrayList.java
@@ -0,0 +1,11 @@
+package day22_ArrayList;
+
+public class IteratingArrayList {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
--- src/utilities/ArraysUtility.java
+++ src/utilities/ArraysUtility.java
@@ -1,5 +1,7 @@
 package utilities;
 
+import java.util.Arrays;
+
 public class ArraysUtility {
 
     public static int[] merge(int[] arr1, int[] arr2){
@@ -123,6 +125,83 @@ public class ArraysUtility {
         return reverse;
     }
 
+    public static int[] addElement(int[] array, int element){
+        int[] new_array = Arrays.copyOf(array, array.length + 1);
+        new_array[new_array.length-1] = element;
+        return new_array;
+    }
+
+
+    public static double[] addElement(double[] array, double element){
+        double[] new_array = Arrays.copyOf(array, array.length + 1);
+        new_array[new_array.length-1] = element;
+        return new_array;
+    }
+
+
+    public static char[] addElement(char[] array, char element){
+        char[] new_array = Arrays.copyOf(array, array.length + 1);
+        new_array[new_array.length-1] = element;
+        return new_array;
+    }
+
+
+    public static String[] addElement(String[] array, String element){
+        String[] new_array = Arrays.copyOf(array, array.length + 1);
+        new_array[new_array.length-1] = element;
+        return new_array;
+    }
+
+    public static boolean contains(int[] array, int element){
+
+        for (int each : array) {
+            if(each == element){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    public static boolean contains(double[] array, double element){
+
+        for (double each : array) {
+            if(each == element){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+
+    public static boolean contains(char[] array, char element){
+
+        for (char each : array) {
+            if(each == element){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+
+    public static boolean contains(String[] array, String element){
+
+        for (String each : array) {
+            if(each.equals( element )){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /*remove and remove dublicate*/
 
 
 
